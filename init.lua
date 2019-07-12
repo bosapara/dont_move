25b6af9eac40a4a0ffdbd6a8172d4e6e231617dd
bosapara <38753015+bosapara@users.noreply.github.com>
2019-07-12 10:57:44 +0400

    update

--- init.lua
+++ init.lua
@@ -2,18 +2,18 @@ minetest.register_chatcommand("spawn2", {
 	description = "Move player back to spawn",
 	privs = {interact = true},
 	func = function(name, param)
-		local player = minetest.get_player_by_name(name) --if player then player:set_hp(0) end
+		local player = minetest.get_player_by_name(name)
 		local pos = player:get_pos()
 		
 		minetest.chat_send_player(name, minetest.colorize("white", "<Mr.Bot> "..name.. ", teleport in process, don't move"))
 		
 		minetest.after(5, function(name, pos1)
 				local player = minetest.get_player_by_name(name)
-				if not player then
-					return --Player isn't online anymore
+				if not player or player:get_hp() == 0 then
+					return --Player isn't online or died
 				end
 				local pos2 = player:get_pos()
-				if not vector.equals(pos1, pos2)
+				if not vector.equals(pos1, pos2) then
 						minetest.chat_send_player(name, minetest.colorize("red", "<Mr.Bot> "..name.. ", you moved, teleport reseted"))
 				else
 						player:setpos({x = 0, y = 10, z = 0})
